9803313e179d862b79ddc18d9952f35150dc995e
3IME <121760193+3IME@users.noreply.github.com>
2023-01-14 16:28:12 +0100

    adding public with categories

--- public/fr/js/bundle.365a404bab3ebf693b4618c367981201982b4ad19da2b4591b4f1b17d99f6234ac22feac3802a0af99b388419def875b4586dc0feb14a0fe6425b9d4fc948177.js
+++ public/fr/js/bundle.365a404bab3ebf693b4618c367981201982b4ad19da2b4591b4f1b17d99f6234ac22feac3802a0af99b388419def875b4586dc0feb14a0fe6425b9d4fc948177.js
@@ -8,7 +8,7 @@ const scaleImageClass = 'image-scale';
 const pageHasLoaded = 'DOMContentLoaded';
 const imageAltClass = 'img_alt'
 
-const baseURL = '';
+const baseURL = 'https://3ime.netlify.app/';
 const searchFieldClass = '.search_field';
 const searchClass = '.search';
 const goBackClass = 'button_back';
